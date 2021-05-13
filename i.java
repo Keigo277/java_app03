2bf93f19ae9b7f022d604a8b30a80c03687506de
柳沢圭吾 <83903345+Keigo277@users.noreply.github.com>
2021-05-13 16:03:10 +0900

    Add files via upload

---
+++ i.java
@@ -0,0 +1,15 @@
+package proje;
+
+ class i {
+
+	public static void main(String[] args) 
+	{
+	 RacingCar rccar1;
+	 rccar1 = new RacingCar();
+	 
+	 rccar1.setCar(1234, 20.5);
+	 rccar1.setCourse(5);
+
+	}
+
+}
